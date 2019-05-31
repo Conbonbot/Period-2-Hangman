999463a96980c0d1806771fe1c49ea904af37a9a
Sanaya <sanaya.bhadha@gmail.com>
2019-05-31 09:19:20 -0700

    Hangman w/ starter updated

--- starter.java
+++ starter.java
@@ -1,11 +1,17 @@
 import pkg.*;
-public class starter
-{
-		
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+
+public class starter {
+	
         public static void main(String args[])
-        {
+		{
+			String a = "AP COMP SCI";
+			Hangman g = new Hangman(a);
 			
 		}
-		
-
+			
 }
+
